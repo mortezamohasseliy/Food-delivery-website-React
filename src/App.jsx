f153ddafe61efaee67345a109351ff320620e842
mortezamohasseliy <murteza.mohasseliy@gmail.com>
2026-08-12 23:59:29 +0430

    settup the project and create header, navbar menu of app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header"
+import NavbarMenu from "./components/NavbarMenu";
+
+function App() {
+  return  <div>
+    <Header />
+    <NavbarMenu />
+  </div>
+}
+
+export default App;
